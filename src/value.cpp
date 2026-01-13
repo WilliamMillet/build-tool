4686bdf1505e520c2aec2d6afc8489d0fbf63bb9
William Millet <z5692883@ad.unsw.edu.au>
2026-01-13 18:23:35 +1100

    Refactor value to store type and use addition

--- src/value.cpp
+++ src/value.cpp
@@ -1,11 +1,60 @@
 #include "value.hpp"
 
-Value::Value(int x) : raw_val(x) { this->type = ValueType::INT; };
+ValueList::ValueList(const ValueList& other) {
+    elements.reserve(other.elements.size());
+    for (const std::unique_ptr<Value>& v : other.elements) {
+        elements.push_back(std::make_unique<Value>(*v));
+    }
+};
 
-Value::Value(std::string x) : raw_val(std::move(x)) { this->type = ValueType::STRING; };
+ValueList& ValueList::operator=(const ValueList& other) {
+    if (this == &other) return *this;
+    ValueList tmp(other);
+    elements.swap(tmp.elements);
+    return *this;
+}
 
-Value::Value(ValueList x) : raw_val(std::move(x)) { this->type = ValueType::LIST; };
+Value::Value() { type = ValueType::NONE; }
 
-Value::Value(ScopedEnumValue x) : raw_val(std::move(x)) { this->type = ValueType::ENUM; };
+Value::Value(int x) : raw_val(x) { type = ValueType::INT; }
 
-ValueType Value::get_type() const { return this->type; };
+Value::Value(std::string x) : raw_val(std::move(x)) { type = ValueType::STRING; }
+
+Value::Value(ValueList x) : raw_val(std::move(x)) { type = ValueType::LIST; }
+
+Value::Value(ScopedEnumValue x) : raw_val(std::move(x)) { type = ValueType::ENUM; }
+
+ValueType Value::get_type() const { return type; }
+
+Value& Value::operator+=(const Value& other) {
+    if (other.type != type) {
+        const std::string type_a = type_string_map.at(type);
+        const std::string type_b = type_string_map.at(type);
+        throw std::invalid_argument("Cannot add two values of opposing types ('" + type_a +
+                                    "' + '" + type_b + "')");
+    }
+
+    switch (type) {
+        case ValueType::INT: {
+            raw_val = std::get<int>(raw_val) + std::get<int>(other.raw_val);
+            break;
+        }
+        case ValueType::STRING: {
+            raw_val = std::get<std::string>(raw_val) + std::get<std::string>(other.raw_val);
+            break;
+        }
+        case ValueType::LIST: {
+            auto& base_list = std::get<ValueList>(raw_val).elements;
+            for (const auto& v : std::get<ValueList>(other.raw_val).elements) {
+                base_list.push_back(std::make_unique<Value>(*v));
+            }
+            break;
+        }
+        default: {
+            std::string type_name = type_string_map.at(type);
+            throw std::invalid_argument("Type '" + type_name + "' does not support addition");
+        }
+    }
+
+    return *this;
+}
--- src/value.hpp
+++ src/value.hpp
@@ -3,22 +3,32 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <variant>
 #include <vector>
 
 class Value;
 
-using ValueList = std::vector<std::unique_ptr<Value>>;
+struct ValueList {
+    std::vector<std::unique_ptr<Value>> elements;
+
+    ValueList() = default;
+
+    ValueList(const ValueList& other);
+
+    ValueList& operator=(const ValueList& other);
+};
 
 struct ScopedEnumValue {
     std::string scope;
     std::string name;
 };
 
-enum class ValueType { INT, STRING, LIST, ENUM };
+enum class ValueType { INT, STRING, LIST, ENUM, NONE };
 
 class Value {
    public:
+    Value();
     Value(int x);
     Value(std::string x);
     Value(ValueList x);
@@ -31,9 +41,18 @@ class Value {
 
     ValueType get_type() const;
 
+    Value& operator+=(const Value& other);
+
    private:
     std::variant<int, std::string, ValueList, ScopedEnumValue> raw_val;
     ValueType type;
+
+    inline static const std::unordered_map<ValueType, std::string> type_string_map = {
+        {ValueType::INT, "Integer"},
+        {ValueType::STRING, "String"},
+        {ValueType::LIST, "List"},
+        {ValueType::ENUM, "Enum"},
+        {ValueType::NONE, "None"}};
 };
 
 #endif
