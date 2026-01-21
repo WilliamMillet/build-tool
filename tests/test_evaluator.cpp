b225b5ceef42244fa852e1ed0a339b74266924a0
William Millet <z5692883@ad.unsw.edu.au>
2026-01-21 14:54:30 +1100

    Write tests for evaluator

--- tests/test_evaluator.cpp
+++ tests/test_evaluator.cpp
@@ -3,6 +3,7 @@
 
 #include "../catch.hpp"
 #include "src/built_in/func_registry.hpp"
+#include "src/dictionaries/rules.hpp"
 #include "src/errors/error.hpp"
 #include "src/parsing/expr.hpp"
 #include "src/parsing/parser.hpp"
@@ -110,10 +111,105 @@ TEST_CASE("Test complex variable dependency DAG", "[variable_evaluator]") {
     REQUIRE(dicts.cfg.get_compiler() == "FooBarBar");
 }
 
-TEST_CASE("Rule parses correctly", "[variable_evaluator]") {}
+TEST_CASE("Rule parses correctly", "[variable_evaluator]") {
+    std::vector<ParsedVariable> vars;
+    vars.push_back({"cfg", Testing::Factories::create_cfg(), VarCategory::CONFIG, {0, 0, 0}});
 
-TEST_CASE("MultiRule parses correctly", "[variable_evaluator]") {}
+    auto rule = std::make_unique<DictionaryExpr>();
 
-TEST_CASE("Clean rule parses correctly", "[variable_evaluator]") {}
+    std::vector<std::string> dep_strings = {"lexer.cpp", "parser.cpp"};
+    std::vector<std::unique_ptr<Expr>> deps;
+    for (const std::string& d : dep_strings) {
+        deps.push_back(std::make_unique<StringExpr>(d));
+    }
+    rule->fields_map[RuleFields::DEPS] = std::make_unique<ListExpr>(std::move(deps));
 
-TEST_CASE("Cyclical variable dependency leads to error", "[variable_evaluator]") {}
+    rule->fields_map[RuleFields::STEP] = std::make_unique<EnumExpr>("Step", "COMPILE");
+
+    vars.push_back({"app", std::move(rule), VarCategory::SINGLE_RULE, {0, 0, 0}});
+
+    VariableEvaluator evaluator(std::move(vars), FuncRegistry());
+    QualifiedDicts dicts = evaluator.evaluate();
+
+    REQUIRE(dicts.rules.size() == 1);
+    std::unique_ptr<Rule>& evaluated = dicts.rules.at(0);
+    REQUIRE(evaluated->get_name() == "app");
+    REQUIRE(evaluated->get_deps() == dep_strings);
+}
+
+TEST_CASE("MultiRule parses correctly", "[variable_evaluator]") {
+    std::vector<ParsedVariable> vars;
+    vars.push_back({"cfg", Testing::Factories::create_cfg(), VarCategory::CONFIG, {0, 0, 0}});
+
+    auto rule = std::make_unique<DictionaryExpr>();
+
+    std::vector<std::string> dep_strings = {"file1.cpp", "file2.cpp", "file3.cpp"};
+    std::vector<std::unique_ptr<Expr>> deps;
+    for (const std::string& d : dep_strings) {
+        deps.push_back(std::make_unique<StringExpr>(d));
+    }
+    rule->fields_map[RuleFields::DEPS] = std::make_unique<ListExpr>(std::move(deps));
+
+    std::vector<std::string> output_strings = {"file1.o", "file2.o", "file3.o"};
+    std::vector<std::unique_ptr<Expr>> outputs;
+    for (const std::string& o : output_strings) {
+        outputs.push_back(std::make_unique<StringExpr>(o));
+    }
+    rule->fields_map[RuleFields::OUTPUT] = std::make_unique<ListExpr>(std::move(outputs));
+
+    rule->fields_map[RuleFields::STEP] = std::make_unique<EnumExpr>("Step", "COMPILE");
+
+    vars.push_back({"compilation", std::move(rule), VarCategory::MULTI_RULE, {0, 0, 0}});
+
+    VariableEvaluator evaluator(std::move(vars), FuncRegistry());
+    QualifiedDicts dicts = evaluator.evaluate();
+
+    REQUIRE(dicts.rules.size() == 1);
+    std::unique_ptr<Rule>& evaluated = dicts.rules.at(0);
+    REQUIRE(evaluated->get_name() == "compilation");
+    REQUIRE(evaluated->get_deps() == dep_strings);
+}
+
+TEST_CASE("Clean rule parses correctly", "[variable_evaluator]") {
+    std::vector<ParsedVariable> vars;
+    vars.push_back({"cfg", Testing::Factories::create_cfg(), VarCategory::CONFIG, {0, 0, 0}});
+
+    auto rule = std::make_unique<DictionaryExpr>();
+
+    std::vector<std::string> target_strings = {"app", "file1.o", "file2.o", "file3.o"};
+    std::vector<std::unique_ptr<Expr>> targets;
+    for (const std::string& t : target_strings) {
+        targets.push_back(std::make_unique<StringExpr>(t));
+    }
+    rule->fields_map[RuleFields::TARGETS] = std::make_unique<ListExpr>(std::move(targets));
+
+    vars.push_back({"clean", std::move(rule), VarCategory::CLEAN, {0, 0, 0}});
+
+    VariableEvaluator evaluator(std::move(vars), FuncRegistry());
+    QualifiedDicts dicts = evaluator.evaluate();
+
+    REQUIRE(dicts.rules.size() == 1);
+    std::unique_ptr<Rule>& evaluated = dicts.rules.at(0);
+    REQUIRE(evaluated->get_name() == "clean");
+    REQUIRE(evaluated->get_deps() == target_strings);
+}
+
+TEST_CASE("Cyclical variable dependency leads to error", "[variable_evaluator]") {
+    /**
+     * a->-b->-c-->-d
+     *  \---<--<---/
+     */
+    std::vector<ParsedVariable> vars;
+
+    std::vector<std::pair<std::string, std::string>> var_setup = {
+        {"b", "a"}, {"c", "b"}, {"d", "c"}, {"d", "a"}};
+
+    for (const auto& [v, dep] : var_setup) {
+        auto a_expr = std::make_unique<VarRefExpr>(dep);
+        ParsedVariable a{v, std::move(a_expr), VarCategory::REGULAR, {0, 0, 0}};
+        vars.push_back(std::move(a));
+    }
+
+    VariableEvaluator evaluator(std::move(vars), FuncRegistry{});
+    REQUIRE_THROWS(evaluator.evaluate());
+}
